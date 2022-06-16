cd688c68d8292e732d0a66c924f006aa0d0aeae7
LEZO100 <106808310+LEZO100@users.noreply.github.com>
2022-06-16 11:05:58 +0200

    Ejemplo 2 del Libro cortesía de NVidia

--- setup.py
+++ setup.py
@@ -1,30 +1,64 @@
-from setuptools import setup, find_packages, Extension
-from torch.utils import cpp_extension
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# plugins_ext = Extension(
-#    name='trt_pose.plugins',
-#    sources=['trt_pose/find_peaks.cpp'],
-#    include_dirs=cpp_extension.include_paths(),
-#    language='c++'
-# )
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
+
+test_requirements = [
+    'tox',
+    'flake8'
+]
 
 setup(
-    name='trt_pose',
-    version='0.0.1',
-    description='Pose detection accelerated by NVIDIA TensorRT',
-    packages=find_packages(),
-    ext_package='trt_pose',
-    ext_modules=[cpp_extension.CppExtension('plugins', [
-        'trt_pose/parse/find_peaks.cpp',
-        'trt_pose/parse/paf_score_graph.cpp',
-        'trt_pose/parse/refine_peaks.cpp',
-        'trt_pose/parse/munkres.cpp',
-        'trt_pose/parse/connect_parts.cpp',
-        'trt_pose/plugins.cpp',
-        'trt_pose/train/generate_cmap.cpp',
-        'trt_pose/train/generate_paf.cpp',
-    ])],
-    cmdclass={'build_ext': cpp_extension.BuildExtension},
-    install_requires=[
+    name='face_recognition',
+    version='1.4.0',
+    description="Recognize faces from Python or from the command line",
+    long_description=readme + '\n\n' + history,
+    author="Adam Geitgey",
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
     ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
